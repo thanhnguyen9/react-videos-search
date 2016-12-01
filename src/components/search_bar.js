7149071cff232819edb94e8763056c0499448435
Thanh Nguyen <thanhnguyen.ldt@gmail.com>
2016-12-01 16:31:47 -0600

    Display thumbnails and title on the page

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -14,7 +14,6 @@ class SearchBar extends Component{
 				<input 
 				value = {this.state.term}
 				onChange={event => this.setState({ term: event.target.value})}/>
-				VALUE: {this.state.term}
 			</div>
 		);
 	}
--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyAuQCVeNfKhtRk9KlChQPT1nO27DPO_5Ss';
 
 //Create a new component. This component should produce some html
 // JSX will get turn into html. Get some js to produce html. JSX make component more clean, easy to read
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
+class App extends Component {
+	constructor(props){
+		super(props);
+
+		this.state = { videos: []};
+
+		YTSearch({key: API_KEY, term: 'surfboard'}, (videos) => {
+			this.setState({ videos });
+		});
+	}
+
+	render(){
+		return (
+			<div>
+				<SearchBar />
+				<VideoList videos={this.state.videos} />
+			</div>
+		);
+	}
 }
 
 // Take this component's generated HTML and put it on the page
