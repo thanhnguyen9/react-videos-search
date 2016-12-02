6b41c9fc5691d6bd816142efae4e5ffdbe81d55f
Thanh Nguyen <thanhnguyen.ldt@gmail.com>
2016-12-01 21:25:42 -0600

    Be able to search for videos

--- src/index.js
+++ src/index.js
@@ -19,7 +19,11 @@ class App extends Component {
 			selectedVideo: null
 		};
 
-		YTSearch({key: API_KEY, term: 'surfboard'}, (videos) => {
+		this.videoSearch('surfboard');
+	}
+
+	videoSearch(term) {
+		YTSearch({key: API_KEY, term: term}, (videos) => {
 			this.setState({ 
 				videos: videos,
 				selectedVideo: videos[0] 
@@ -30,7 +34,7 @@ class App extends Component {
 	render(){
 		return (
 			<div>
-				<SearchBar />
+				<SearchBar onSearchTermChange={term => this.videoSearch(term)} />
 				<VideoDetail video={this.state.selectedVideo} />
 				<VideoList 
 					onVideoSelect={selectedVideo => this.setState({selectedVideo})}
